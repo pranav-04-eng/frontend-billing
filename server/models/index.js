5d13efcc94d253075ba42856816e0d416b6f1c7a
Pranav S <139418163+pranav-04-eng@users.noreply.github.com>
2025-07-08 14:35:36 +0530

    Delete server directory

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-import User from './User.js';
-import Invoice from './Invoice.js';
-
-// Define associations if needed
-// User.hasMany(Invoice, { foreignKey: 'customerEmail', sourceKey: 'email' });
-// Invoice.belongsTo(User, { foreignKey: 'customerEmail', targetKey: 'email' });
-
-export { User, Invoice };
